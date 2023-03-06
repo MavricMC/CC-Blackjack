ec9d4f0ffaa4cc4a894d0ae0508a041553cc9254
MavricMC <118589100+MavricMC@users.noreply.github.com>
2023-03-06 13:40:32 +1300

    Create pinapi.lua

---
+++ pinapi.lua
@@ -0,0 +1,86 @@
+--Blackjack version 0.0.5--
+
+--Edited by Mavric on YouTube--
+
+--Made by Bloodmuffin on YoutTube--
+local input = ""
+local tig = ""
+local keypad = {
+--{num,xpos,ypos}
+    {1, 20, 20},
+    {2, 25, 20},
+    {3, 30, 20},
+    {4, 20, 22},
+    {5, 25, 22},
+    {6, 30, 22},
+    {7, 20, 24},
+    {8, 25, 24},
+    {9, 30, 24},
+    {10, 20, 26}, --clear
+    {0, 25, 26},
+    {11, 30, 26} --enter
+}
+
+-----FUNCTIONS-----
+function checkClick(t, x, y)
+    for i,v in pairs(t) do
+        local num, cx, cy = unpack(v)
+        if (x >= cx) and (x <= cx + 2) and (y == cy) then
+	        return true, num
+	    end
+    end
+  return false
+end
+
+function numClick(num)
+    if num ==  11 then
+        return true, 0
+    elseif num == 10 then
+        return true, 1
+    else
+        input = input .. num
+        term.write(tostring(num))
+        return false
+    end
+end
+
+--Main program--
+function pinpad(xx, yy)
+    tig = ""
+    input = ""
+    term.setBackgroundColor(colors.brown)
+    term.setTextColor(1)
+    for i, v in pairs(keypad) do
+        local num, x, y = unpack(v)
+        term.setCursorPos(x, y)
+        if num == 10 then
+            term.setBackgroundColor(colors.red)
+            term.write(" " .. "C" .. " ")
+        elseif num == 11 then
+            term.setBackgroundColor(colors.green)
+            term.write(" " .. "E" .. " ")
+        else
+            term.setBackgroundColor(colors.brown)
+            term.write(" " .. num .. " ")
+        end
+    end
+    term.setBackgroundColor(colors.green)
+    term.setTextColor(1)
+    term.setCursorPos(xx, yy)
+    while true do
+        local event, button, x, y = os.pullEvent("monitor_touch")
+        local validClick, num = checkClick(keypad, x, y)
+        if (validClick) then
+            local triger, but = numClick(num)
+            if (triger) then
+                if but == 0 then
+                    tig = input
+                    input = ""
+                    return true, tig
+                elseif but == 1 then
+                    return false
+                end
+            end
+        end
+    end
+end
